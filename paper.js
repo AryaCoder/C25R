c47c628a8f4eb6eba7e721592e48a450b41f2fae
AryaCoder <75891256+AryaCoder@users.noreply.github.com>
2021-05-05 15:41:14 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,36 @@
+class Paper{
+    constructor(x,y,r){
+        var options = {
+            isStatic : false,
+			restitution : 0.3,
+            friction : 0,
+            density : 1.2
+        }
+       
+        this.x=x;
+		this.y=y;
+		this.r=r
+		this.body=Bodies.circle(this.x, this.y, (this.r-20)/2, options)
+		this.image=loadImage("paper.png")
+        World.add(world, this.body);
+
+	}
+	display()
+	{
+			var rubberpos=this.body.position;		
+			push()
+			translate(rubberpos.x, rubberpos.y);
+			rectMode(CENTER)
+			strokeWeight(4);
+			stroke(230);
+			fill(230);
+		
+			imageMode(CENTER);
+        image(this.image,0,0,this.r,this.r);
+			
+            //draw the ellipse here to display the rubber ball
+            
+			pop()
+	}
+
+}
